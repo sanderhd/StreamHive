4999b13668d691eacd5fd948c04aacb2098d89fc
Sander <fristislekker@proton.me>
2026-06-02 14:32:08 +0200

    git: remove config.php and add example

---
+++ config/Config.exampe.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    "app_name" => "StreamHive",
+
+    "base_path" => "/streamhive",
+
+    "database" => [
+        "host" => "",
+        "dbname" => "",
+        "username" => "",
+        "password" => ""
+    ],
+
+    // Cloudflare
+    "TURNSTILE_SECRET" => ""
+];
